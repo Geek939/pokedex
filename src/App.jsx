c7fe1042ec8a743575a1514cd0dd0328477e6e96
JRoberto <kanon939@hotmail.com>
2022-12-23 01:21:31 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Pokedex from './pages/Pokedex'
 
+
 function App() {
   
 
@@ -10,8 +11,12 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element = {<Home/>} />
+
+        <Route element={} >
         <Route path='/pokedex' element = {<Pokedex/>} />
         <Route path='/pokedex/:id' element = {<Pokedex/>} />
+        </Route>
+       
      </Routes>
        
     </div>
---
+++ src/components/RouteProtected.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RouteProtected = () => {
+  return (
+    <div>RouteProtected</div>
+  )
+}
+
+export default RouteProtected
