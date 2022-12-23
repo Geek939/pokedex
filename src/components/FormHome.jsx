24f917cb509e804a1eaf7b32faf839a223c3f136
JRoberto <kanon939@hotmail.com>
2022-12-23 01:17:16 -0700

    update

--- src/components/FormHome.jsx
+++ src/components/FormHome.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { setNameTrainerGlobal } from '../store/slices/nameTrainer.slice'
+import {useDispatch} from 'react-redux'
 
 const FormHome = () => {
   
@@ -8,13 +9,13 @@ const FormHome = () => {
 
   const handleSubmit =(e) =>{
     e.preventDefault()
-  const nameTrainer =  e.target.nameTrainer.value
+  const nameTrainer =  e.target.nameTrainer.value.trim()
     dispatch(setNameTrainerGlobal(nameTrainer))
   }
   return (
     
     <form className='home__form' onSubmit={handleSubmit}>
-        <input  className='home__input' type="text" placeholder='Your name...'/>
+        <input required className='home__input' type="text" id='nameTrainer' placeholder='your name...'/>
         <button className='home__btn' >Start!</button>
     </form>
 
