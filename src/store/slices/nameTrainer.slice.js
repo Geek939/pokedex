5f9156cd6b26710c7d35bb6b65d49581fa31f6c3
JRoberto <kanon939@hotmail.com>
2022-12-23 00:33:42 -0700

    global state

---
+++ src/store/slices/nameTrainer.slice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+ const nameTrainerSlice= createSlice({
+
+name:"nameTrainer",
+initialState:"",
+reducers:{
+
+
+}
+
+})
+
+export default nameTrainerSlice.reducer
