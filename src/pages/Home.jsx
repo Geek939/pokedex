b4226c45c976ca9ce4d07cbf9babd9202ad1094b
JRoberto <kanon939@hotmail.com>
2022-12-23 10:27:45 -0700

    font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import FormHome from '../components/FormHome'
+import "./styles/Home.css"
 
 const Home = () => {
   return (
 
     <main className='home'>
-    <img className='home__img' src="" alt="" />    
+    <img className='home__img' src="/images/tittle.png" alt="" />    
     <h2 className='home__subtitle' >hi, trainer!</h2>
     <p className='home__text' >Give your name to start!</p>
     <FormHome/>
