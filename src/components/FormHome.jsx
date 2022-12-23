4ab9f3e571c71f557983671666d2fc6d935766c3
JRoberto <kanon939@hotmail.com>
2022-12-23 00:55:10 -0700

    update

--- src/components/FormHome.jsx
+++ src/components/FormHome.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { setNameTrainerGlobal } from '../store/slices/nameTrainer.slice'
 
 const FormHome = () => {
+  
+
+  const dispatch =  useDispatch()
+
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+  const nameTrainer =  e.target.nameTrainer.value
+    dispatch(setNameTrainerGlobal(nameTrainer))
+  }
   return (
     
-    <form className='home__form' >
+    <form className='home__form' onSubmit={handleSubmit}>
         <input  className='home__input' type="text" placeholder='Your name...'/>
         <button className='home__btn' >Start!</button>
     </form>
