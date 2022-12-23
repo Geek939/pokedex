62cac1085c2ff5d186ad29d8b46c416e9fbfd9c1
JRoberto <kanon939@hotmail.com>
2022-12-22 23:40:14 -0700

    update

---
+++ src/components/FormHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FormHome = () => {
+  return (
+    
+    <form className='home__form' >
+        <input  className='home__input' type="text" placeholder='Your name...'/>
+        <button className='home__btn' >Start!</button>
+    </form>
+
+  )
+}
+
+export default FormHome
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+
+    <main className='home'>
+    <img className='home__img' src="" alt="" />    
+    <h2 className='home__subtitle' >hi, trainer!</h2>
+    <p className='home__text' >Give your name to start!</p>
+    <form className='home__form' >
+    <input  className='home__input' type="text" placeholder='Your name...'/>
+    <button className='home__btn' >Start!</button>
+    </form>
+    </main>
   )
 }
 
