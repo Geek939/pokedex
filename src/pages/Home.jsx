3d1ff806ed636655f7b470f5e8222a5c7474aa94
JRoberto <kanon939@hotmail.com>
2022-12-22 23:52:59 -0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import FormHome from '../components/FormHome'
 
 const Home = () => {
   return (
@@ -7,10 +8,7 @@ const Home = () => {
     <img className='home__img' src="" alt="" />    
     <h2 className='home__subtitle' >hi, trainer!</h2>
     <p className='home__text' >Give your name to start!</p>
-    <form className='home__form' >
-    <input  className='home__input' type="text" placeholder='Your name...'/>
-    <button className='home__btn' >Start!</button>
-    </form>
+    <FormHome/>
     </main>
   )
 }
