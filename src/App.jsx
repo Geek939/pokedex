2345a5c74b518d3427905d546bc3764345cf953f
JRoberto <kanon939@hotmail.com>
2022-12-22 23:28:42 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Home from './pages/Home'
+import Pokedex from './pages/Pokedex'
 
 function App() {
   
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/pokedex' element = {<Pokedex/>} />
+        <Route path='/pokedex/:id' element = {<Pokedex/>} />
+     </Routes>
        
     </div>
   )
