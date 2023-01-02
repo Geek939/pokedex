ad0f3a3ee425c28b57580f0e35d3dea8efd069d6
JRoberto <kanon939@hotmail.com>
2023-01-01 20:37:56 -0700

    pokemonCard

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -10,7 +10,7 @@ const Pokedex = () => {
   const nameTrainer = useSelector(state => state.nameTrainer)
 
   useEffect(() => {
-    const URL = "https://pokeapi.co/api/v2/pokemon/?limit=1154"
+    const URL = "https://pokeapi.co/api/v2/pokemon/?limit=15" /*1154*/
     axios.get(URL)
     .then(res => setPokemons(res.data.results))
     .catch(err => console.log(err))
